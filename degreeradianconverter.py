5e4b64dc4aa24076f1d9df734a785525fca4a539
Josh Guertler <93700477+greped@users.noreply.github.com>
2022-04-06 01:40:42 -0400

    Update degreeradianconverter.py

--- degreeradianconverter.py
+++ degreeradianconverter.py
@@ -1,5 +1,7 @@
 import math
 def main():
+    print("This script will help you convert radians to degrees or degrees to radians. Please make sure that you follow the instructions below and only\n"
+    print("input the value and the word radian or degrees, whichever unit your measurement is in.\n"
     prompt=input("Type the value followed by the word degrees or radians, will solve for other unit: ")
     unit, val = prompt.split(" ")
     if val == 'degrees':
