ea7796dd9ddf5c15f4a6c2a6879b65310d0e9b4f
Josh Guertler <93700477+greped@users.noreply.github.com>
2022-04-10 23:15:30 -0400

    Update intometersadvanced.py

--- intometersadvanced.py
+++ intometersadvanced.py
@@ -1,3 +1,5 @@
+#This script converts inches to meters. If you want to use multiple inputs, please space them with a comma and only write the values (i.e. don't add "in." or "inches"
+#You do not need to simplify your inputs (i.e. an input of 10+(7/10) will be treated as 10.7)
 inInput = input("write numbers here: ").split(",")
 
 happyList = []
@@ -5,4 +7,4 @@ happyList = []
 for numStr in inInput:
     happyList.append(float(eval(numStr))*0.0254)
 
-print(happyList,"meters")
+print("The following are the values in meters", happyList)
